946d2448df820a1c6a13afe8877d13765724ffa4
terkmail <terkmail@gmail.com>
2020-02-10 11:27:35 +0200

    added themes pic

--- GUI.py
+++ GUI.py
@@ -1,31 +1,36 @@
 import PySimpleGUI as sg
-import os
 
-mytext = 'my text...'
+sg.ChangeLookAndFeel('TealMono')
 
-# All the stuff inside your window. 
-layout = [  [sg.Button('Build Lib')], 
-            [sg.Button('Build Test Lib')], 
-            [sg.Button('Build Test App')], 
-            [sg.Button('Exit')],
-            [sg.Text(mytext)] ]
-# Create the Window
-window = sg.Window('MyGUI', layout)
-# Event Loop to process "events" and get the "values" of the inputs
-while True:             
-    event, values = window.read()
-    if event in (None, 'Exit'):  
-        break
-    if event in (None, 'Build Lib'):   
-        os.system("echo building lib...")
-        os.system("date /t > mylib.a")
-        mytext = 'done'
-        os.system("echo done")
-    if event in (None, 'Build Test Lib'):   
-        os.system("echo building lib...")
-        os.system("echo fail")
-    if event in (None, 'Build Test App'):   
-        print(values)
-    
+column1 = [[sg.Text('Column 1', background_color='#d3dfda', justification='center', size=(10, 1))],      
+           [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box 1')],      
+           [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box 2')],      
+           [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box 3')]]      
+layout = [      
+    [sg.Text('All graphic widgets in one window!', size=(30, 1), font=("Helvetica", 25))],      
+    [sg.Text('Here is some text.... and a place to enter text')],      
+    [sg.InputText('This is my text')],      
+    [sg.Checkbox('My first checkbox!'), sg.Checkbox('My second checkbox!', default=True)],      
+    [sg.Radio('My first Radio!     ', "RADIO1", default=True), sg.Radio('My second Radio!', "RADIO1")],      
+    [sg.Multiline(default_text='This is the default Text should you decide not to type anything', size=(35, 3)),      
+     sg.Multiline(default_text='A second multi-line', size=(35, 3))],      
+    [sg.InputCombo(('Combobox 1', 'Combobox 2'), size=(20, 3)),      
+     sg.Slider(range=(1, 100), orientation='h', size=(34, 20), default_value=85)],      
+    [sg.Listbox(values=('Listbox 1', 'Listbox 2', 'Listbox 3'), size=(30, 3)),      
+     sg.Slider(range=(1, 100), orientation='v', size=(5, 20), default_value=25),      
+     sg.Slider(range=(1, 100), orientation='v', size=(5, 20), default_value=75),      
+     sg.Slider(range=(1, 100), orientation='v', size=(5, 20), default_value=10),      
+     sg.Column(column1, background_color='#d3dfda')],      
+    [sg.Text('_'  * 80)],      
+    [sg.Text('Choose A Folder', size=(35, 1))],      
+    [sg.Text('Your Folder', size=(15, 1), auto_size_text=False, justification='right'),      
+     sg.InputText('Default Folder'), sg.FolderBrowse()],      
+    [sg.Submit(), sg.Cancel()]      
+]
 
-window.close()
+form = sg.FlexForm("Change Combo Values", default_element_size=(12,1), text_justification='r', auto_size_text=False, auto_size_buttons=False,
+                   default_button_element_size=(12,1))
+
+window = sg.Window('Everything bagel', default_element_size=(40, 1)).Layout(layout)
+button, values = window.Read()
+sg.Popup(button, values)
